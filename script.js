0455d74c9fe9a872a05caf3e686a2c3c38ffe8db
Miwan9 <kozhema7@gmail.com>
2017-12-06 12:54:08 +0200

    second

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //Zadacha 1
-// function redcol(element){
-// 	document.getElementsByTagName('p')[0].style.backgroundColor='red';
-// 	document.getElementsByTagName('p')[1].style.backgroundColor='red';
-// 	document.getElementsByTagName('p')[2].style.backgroundColor='red';
-// }
-// redcol();	
+function redcol(element){
+	document.getElementsByTagName('p')[0].style.backgroundColor='red';
+	document.getElementsByTagName('p')[1].style.backgroundColor='red';
+	document.getElementsByTagName('p')[2].style.backgroundColor='red';
+}
+redcol();	
 
 //Zadacha 2
-// function makeShadow (){
-// 	document.getElementsByTagName('img')[0].style.boxShadow='5px 5px 10px 3px';
-// 	document.getElementsByTagName('img')[1].style.boxShadow='5px 5px 10px 3px';
-// }
-// makeShadow();
+function makeShadow (){
+	document.getElementsByTagName('img')[0].style.boxShadow='5px 5px 10px 3px';
+	document.getElementsByTagName('img')[1].style.boxShadow='5px 5px 10px 3px';
+}
+makeShadow();
 
 //Zadacha 3
 // function makeNumbers(){
@@ -23,17 +23,17 @@
 // makeNumbers();
 
 //Zadacha4
-// var but=document.getElementById('btn1');
-// but.onclick=function (){
-// 	console.log('Действие');
-// }
+var but=document.getElementById('btn1');
+but.onclick=function (){
+	console.log('Действие');
+}
 
 //Zadacha 5
-// var a=Math.pow(3,3);
-// function matem(){
-// 	alert(a);
-// }
-// matem();
+var a=Math.pow(3,3);
+function matem(){
+	alert(a);
+}
+matem();
 
 //Zadacha 6
 var tg=document.getElementsByTagName('p')[0];
